34b8c20ff4fca707e709ada629b358c49ae7e79a
Dev Mehta <dev.a.mehta30@gmail.com>
2023-07-07 22:55:29 +0530

    Data Input

---
+++ portfolio/src/components/Sections.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Background from "../assets/Below_Project.png"
+
+function Sections(){
+    return(
+        <>
+        <img src={Background} alt="NB" />
+        </>
+    )
+}
+
+export default Sections;
--- portfolio/src/pages/data/index.jsx
+++ portfolio/src/pages/data/index.jsx
@@ -1,37 +1,80 @@
 import React from "react";
 import { useState } from "react";
 import { storage } from "../../firebase";
-import { ref,uploadBytes } from "firebase/storage";
+import { ref, uploadBytes } from "firebase/storage";
+import { database } from "../../firebase";
+import { push, child, update } from "firebase/database";
 function Index() {
+  const [name, setName] = useState(null);
+  const [desc, setDesc] = useState(null);
   const [image, setImage] = useState(null);
+
+  const handleSubmit = () => {
+    let obj = {
+      name: name,
+      desc: desc,
+    };
+    const newPostKey = push(child(ref(database), "posts")).key;
+    const updates = {};
+    updates["/" + newPostKey] = obj;
+    return update(ref(database), updates);
+  };
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "name") {
+      setName(value);
+    }
+    if (id === "desc") {
+      setDesc(value);
+    }
+  };
   const uploadImage = () => {
     if (image == null) return;
     const imageRef = ref(storage, `${image.name}`);
-    uploadBytes(imageRef,image).then(()=>{
-        alert("Submitted");
+    uploadBytes(imageRef, image).then(() => {
+      alert("Submitted");
     });
   };
   return (
     <>
       {/* <form action=""> */}
-        {/* <label htmlFor="">
-          Enter the Project Name:
-          <input type="text" />
-        </label>
-        <label htmlFor="">
-          Enter the description for the Project:
-          <input type="text" />
-        </label> */}
-        <div htmlFor="">
-          Give the the cover image
-          <input
-            type="file"
-            onChange={(event) => {
-              setImage(event.target.files[0]);
-            }}
-          />
-        </div>
-        <button onClick={uploadImage}>Submit</button>
+      <label htmlFor="">
+        Enter the Project Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => handleInputChange(e)}
+          id="name"
+          placeholder="Name"
+        />
+      </label>
+      <label htmlFor="">
+        Enter the description for the Project:
+        <input
+          type="text"
+          value={desc}
+          onChange={(e) => handleInputChange(e)}
+          id="desc"
+          placeholder="Description"
+        />
+      </label>
+      <div htmlFor="">
+        Give the the cover image
+        <input
+          type="file"
+          onChange={(event) => {
+            setImage(event.target.files[0]);
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          uploadImage();
+          handleSubmit();
+        }}
+      >
+        Submit
+      </button>
       {/* </form> */}
     </>
   );
